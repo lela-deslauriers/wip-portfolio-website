de95e027e8207ad36965d2ef057cdc1464250233
Lela Deslauriers <lela.deslauriers@gmail.com>
2022-10-20 17:10:27 -0700

    updated script.js, updated HTML structure and content

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Welcome to my coding playground!";
+h2.textContent = "Learning to code is part of my product management journey.";
 
 document.querySelector("body").appendChild(h2);
